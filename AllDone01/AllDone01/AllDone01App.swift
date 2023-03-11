6c0309ddd814db9eeb833c6e2a5d0775dd0be241
Takaomi Yukawa <omi.yukawa@gmail.com>
2023-03-11 16:36:01 +0900

    Setup Project Done

---
+++ AllDone01/AllDone01/AllDone01App.swift
@@ -0,0 +1,17 @@
+//
+//  AllDone01App.swift
+//  AllDone01
+//
+//  Created by 湯川隆臣 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct AllDone01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllDone01/AllDone01/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  AllDone01
+//
+//  Created by 湯川隆臣 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+    
