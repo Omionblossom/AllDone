78bb5c202abd3dde78643ba1021731fd17fec513
Takaomi Yukawa <omi.yukawa@gmail.com>
2023-03-11 20:39:11 +0900

    Sign up view added

--- AllDone01/AllDone01/View/Authentication/SignupView.swift
+++ AllDone01/AllDone01/View/Authentication/SignupView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct SignupView: View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var email = ""
+    @State var password = ""
+    @State var confirmPassword = ""
+    
+    @Environment(\.presentationMode) var mode
+    
     var body: some View {
  
         ZStack{
             BackgroundGradientView()
-        }
+            
+            VStack {
+                LogoView()
+                    .padding(.bottom,25)
+                
+                VStack(spacing: 20) {
+                    
+                    UserTextField(text: $firstName, placeholder: "First Name")
+                    UserTextField(text: $lastName, placeholder: "Last Name")
+                    EmailTextField(text: $email)
+                    PasswordSecureField(text: $password, placeholder: "Password")
+                    PasswordSecureField(text: $confirmPassword, placeholder: "Confirm Password")
+                    
+                } // end of VStack
+                .padding(.horizontal, 32)
+                
+                Button {
+                    // Sign up Action
+                } label: {
+                    AuthenticateButtonView(text: "Sign Up")
+                        .padding()
+                    
+                }
+                Spacer()
+                
+                Button {
+                    mode.wrappedValue.dismiss()
+                } label: {
+                    HStack{
+                        Text("Already have an account?")
+                            .font(.system(size: 14))
+                        Text("Sign In")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.gray)
+                }
+                .padding(.bottom, 16)
+
+                
+            }// end of VStack
+            .padding(.top, 20)
+            
+        } // end of ZStack
     }
 }
 
---
+++ AllDone01/AllDone01/View/Utils/TextFields/UserTextField.swift
@@ -0,0 +1,32 @@
+//
+//  UserTextField.swift
+//  AllDone01
+//
+//  Created by 湯川隆臣 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct UserTextField: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    
+    var body: some View {
+        CustomTextField(text: $text, placeholder: Text(placeholder), imageName: "person")
+            .padding()
+            .background(Color(.init(white:1, alpha:0.15)))
+            .cornerRadius(10)
+            .foregroundColor(.white)
+    }
+}
+
+struct UserTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            BackgroundGradientView()
+            UserTextField(text: .constant(""), placeholder: "User")
+        }
+    }
+}
+
