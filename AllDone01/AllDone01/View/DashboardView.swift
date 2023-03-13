9e48bff337ab742c70c76f2f3c9ebb702c753f69
Takaomi Yukawa <omi.yukawa@gmail.com>
2023-03-13 16:15:11 +0900

    Dashboard started and searchBoard added

--- AllDone01/AllDone01/View/DashboardView.swift
+++ AllDone01/AllDone01/View/DashboardView.swift
@@ -10,20 +10,55 @@ import SwiftUI
 struct DashboardView: View {
     
     let user: AppUser
+    
+    @State var searchText: String = ""
 
+    @ObservedObject var viewModel = TODOViewModel()
     
     var body: some View {
-        VStack {
-            Text("DashboardView")
-            Button {
-                AuthViewModel.shared.signout()
-            } label: {
-                Text("Log Out")
-            }
-            .padding()
+        
+        ZStack {
             
-
-        }
+            VStack{
+                HStack {
+                    Spacer()
+                    Text("- AllDone -")
+                        .font(.system(size: 24))
+                        .fontWeight(.semibold)
+                    Spacer()
+                } // end of HStack
+                .overlay {
+                    HStack {
+                        Spacer()
+                        Button {
+                            AuthViewModel.shared.signout()
+                        } label: {
+                            Text("Logout")
+                                .foregroundColor(Color(.systemGray))
+                        }
+                        .padding(.trailing)
+                    }
+                } // end of HStack overlay
+                
+                SearchBarView(searchText: $searchText)
+                    .padding()
+                
+                
+            } // end of VStack
+            
+        } // end of Zstack
+        
+//        VStack {
+//            Text("DashboardView")
+//            Button {
+//                AuthViewModel.shared.signout()
+//            } label: {
+//                Text("Log Out")
+//            }
+//            .padding()
+//
+//
+//        }
     }
 }
 
---
+++ AllDone01/AllDone01/View/Utils/SearchBarView.swift
@@ -0,0 +1,89 @@
+//
+//  SearchBarView.swift
+//  AllDone01
+//
+//  Created by 湯川隆臣 on 2023/03/13.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    @State var isSearching = false
+    
+    var body: some View {
+
+        HStack {
+            TextField("Search TODO", text: $searchText)
+                .searchTextFieldStyle()
+                .onTapGesture(perform: {
+                    isSearching = true
+                })
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("lightBlue"))
+                        Spacer()
+                        
+                        if isSearching{
+                            Button {
+                                searchText = ""
+                            } label: {
+                                Image(systemName: "xmark.circle.fill")
+                            }
+                            
+                        }
+                    }
+                        .padding(.horizontal, 8)
+                        .foregroundColor(.gray)
+                    
+                )
+                .transition(.move(edge: .trailing))
+            
+            if isSearching{
+                Button(action: {
+                    isSearching = false
+                    searchText = ""
+                    
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(Color("lightBlue"))
+                        .padding(.trailing)
+                        .padding(.leading, 0)
+                })
+                .transition(.move(edge: .trailing))
+                
+            }
+        }
+        
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            BackgroundGradientView()
+            SearchBarView(searchText: .constant(""))
+        }
+    }
+}
+
+struct SearchTextFieldModifier: ViewModifier {   // creating own style
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 17))
+            .padding(.leading, 24)
+            .padding(.horizontal)
+            .frame(height: 45)
+            .background(Color(.systemGray6))
+            .cornerRadius(15)
+    }
+}
+
+extension View {
+    func searchTextFieldStyle() -> some View {
+        modifier(SearchTextFieldModifier())
+    }
+}
